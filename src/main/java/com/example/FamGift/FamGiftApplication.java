c38c416dbc86b739dc20da948d1ac914e74c5997
201724554 <pay7845@naver.com>
2023-09-16 23:05:15 +0900

    project settting

---
+++ src/main/java/com/example/FamGift/FamGiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.FamGift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamGiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FamGift/FamGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FamGift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
